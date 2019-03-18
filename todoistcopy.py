340d36390fbc36719217e2580f7da7de2046ca7e
IliaNov <volok2@ya.ru>
2019-03-19 00:08:35 +0300

    правки после инспекции кода

--- todoistcopy.py
+++ todoistcopy.py
@@ -1,10 +1,10 @@
 import sqlite3
 
 
-
 class dbase:
     conn = None
     cur = None
+
     def connect(self):
         self.conn = sqlite3.connect("todoistcopydb.db")
         self.cur = self.conn.cursor()
@@ -14,18 +14,19 @@ class dbase:
         self.cur.close()
         self.conn.close()
 
+
 class user(dbase):
     id = 0
+
     def __init__(self, login, password):
         if not self.authorise(login, password):
-            return None;
-
+            return None
 
     def authorise(self, login, password):
         self.connect()
         self.cur.execute("SELECT id FROM user WHERE name=? and password=?", (login, password))
         id = self.cur.fetchone()[0]
-        if id !=0:
+        if id != 0:
             self.id = id
             self.connectclose()
             return True
@@ -33,20 +34,19 @@ class user(dbase):
             self.connectclose()
             return False
 
-    def addtask(self, task):
+    def addtask(self, taskobj):
         self.connect()
-        self.cur.execute("INSERT INTO usertask (userid, taskid) VALUES (?,?)", (self.id, task.id))
+        self.cur.execute("INSERT INTO usertask (userid, taskid) VALUES (?,?)", (self.id, taskobj.id))
         self.connectclose()
-
-
-
 
 
 class task(dbase):
     id = 0
-    def __init__(self, name, descr = "NULL", dateterm = "NULL", timeterm = "NULL"):
+
+    def __init__(self, name, descr="NULL", dateterm="NULL", timeterm="NULL"):
         self.connect()
-        self.cur.execute("INSERT INTO task (name, descr, dateterm, timeterm) VALUES (?,?,?,?)",(name, descr, dateterm, timeterm))
+        self.cur.execute("INSERT INTO task (name, descr, dateterm, timeterm) VALUES (?,?,?,?)",
+                         (name, descr, dateterm, timeterm))
         self.id = self.cur.execute("SELECT id FROM task WHERE rowid=last_insert_rowid()").fetchone()[0]
         self.connectclose()
 
@@ -57,5 +57,5 @@ class project(dbase):
 
 
 usr = user('vasya', 'vasya')
-#tsk = task("novay", "opis")
-#usr.addtask(tsk)
+tsk = task("novay", "opis")
+usr.addtask(tsk)
